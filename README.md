44b99f018634b0df04b723bb64e1f11ee6b15d00
wangkaiwd <1162486176@qq.com>
2021-08-10 00:09:40 +0800

    feat: init vite project by ant design vue

---
+++ README.md
@@ -0,0 +1,5 @@
+## Vue 3 + Typescript + Vite
+
+### 启动项目
+
+* [start error](https://github.com/evanw/esbuild/issues/462#issuecomment-771328459)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a-button type="primary">按钮</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+});
+</script>
+
+<style>
+</style>
